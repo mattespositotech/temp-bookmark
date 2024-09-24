cfbdb02467ce8fba5d29b378fc3b111e279f9b50
mattespositotech <matt@teamduality.dev>
2024-09-23 20:24:06 -0500

    Permissions for Background worker

--- background.js
+++ background.js
@@ -1,13 +1,12 @@
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(() => {
   chrome.alarms.create("cleanupBookmarks", { periodInMinutes: 60 });
 });
 
-chrome.alarms.onAlarm.addListener(function (alarm) {
+chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "cleanupBookmarks") {
-    chrome.storage.local.get({ tempBookmarks: [] }, function (result) {
-      let tempBookmarks = result.tempBookmarks;
-      let now = Date.now();
-      let updatedBookmarks = tempBookmarks.filter(
+    chrome.storage.local.get({ tempBookmarks: [] }, ({ tempBookmarks }) => {
+      const now = Date.now();
+      const updatedBookmarks = tempBookmarks.filter(
         (bookmark) => bookmark.expiration > now
       );
 
